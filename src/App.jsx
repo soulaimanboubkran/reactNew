22829b51a2a926b131b27c428fb16a345f8755a8
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2023-12-13 00:22:34 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TableData from './table'
 function App() {
   const [res,setRes] =useState([])
   const [formData, setFormData] = useState({
-      
+      id:Date.now(),
       nom:'',
       ville: '',
     });
@@ -25,33 +25,82 @@ function App() {
       
           e.preventDefault()
          
-  
-  const data={
-      id:res.length+1,
+  try {
+    const data={
+      id:Date.now()+1,
       formData:{...formData},
   }
-  setRes([...res,data])
+   setRes([...res,data])
    setFormData({
+    id:Date.now()+1,
       nom:'',
       ville: '',
   })
+  } catch (error) {
+    console.log(error)
   }
   
+  }
+
+
+  const handleDelete = (id) => {
+  try {
+    const updatedRes = res.filter((item) => item.id !== id);
+    setRes(updatedRes);
+  } catch (error) {
+    console.log(error)
+  }
+    
+  };
  
   return (
     <>
-       <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text"name='nom' value={formData.nom} onChange={handleChange}/>
-        <input type="text"name='ville'value={formData.ville} onChange={handleChange}/>
-        <button type='submit'>create</button>
+        <div className="p-4 rounded-md bg-gray-100">
+      <form onSubmit={handleSubmit} className="mb-4 flex flex-wrap items-center"> 
+        <label htmlFor="nom" className="mr-2">Nom:</label>
+        <input
+          type="text"
+          id="nom"
+          name="nom"
+          value={formData.nom}
+          onChange={handleChange}
+          className="p-2 border rounded-md mb-2 md:mb-0 md:mr-2"
+        />
+
+        <label htmlFor="ville" className="mr-2">Ville:</label>
+        <input
+          type="text"
+          id="ville"
+          name="ville"
+          value={formData.ville}
+          onChange={handleChange}
+          className="p-2 border rounded-md mb-2 md:mb-0 md:mr-2" 
+        />
+
+        <button type="submit" className="bg-blue-600 text-white p-2 rounded-md mx-2 md:mx-4"> {/* Responsive margin */}
+          Create
+        </button>
       </form>
-      <ul>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+ <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+      <tr className="bg-gray-200">
+        <th className="py-2 px-4 border-b">ID</th>
+        <th className="py-2 px-4 border-b">Nom</th>
+        <th className="py-2 px-4 border-b">Ville</th>
+        <th className="py-2 px-4 border-b"></th>
+      </tr>
+    </thead>
+      
         {res.map((item) => (
-         <TableData  item={item.formData}/>
-         
+          <TableData item={item.formData} key={item.id}
+          handleUpdate={() => handleUpdate(item.id)}
+          handleDelete={() => handleDelete(item.id)}
+          handleView={() => handleView(item.id)}
+            />
         ))}
-      </ul>
+      </table>
+      </div>
     </div>
     
     </>
--- src/table.jsx
+++ src/table.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
 
-const TableData = ({item}) => {
-  return (
-    <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Nom</th>
-        <th>Ville</th>
-      </tr>
-    </thead>
-    <tbody>
-      
-        <tr key={item.id}>
-          <td>{item.id}</td>
-          <td>{item.nom}</td>
-          <td>{item.ville}</td>
-        </tr>
+const TableData = ({item, handleUpdate,handleDelete }) => {
 
+  return (
+   
+    <tbody>
+      <tr key={item.id} className="hover:bg-gray-100">
+        <td className="py-2 px-4 border-b">{item.id}</td>
+        <td className="py-2 px-4 border-b">{item.nom}</td>
+        <td className="py-2 px-4 border-b">{item.ville}</td>
+        <td class="px-6 py-4 text-center ">
+                    <a  className="font-medium mx-1 text-blue-600 dark:text-blue-500 cursor-pointer " onClick={()=>handleUpdate(item)} >Edit</a>
+                    <a className="font-medium mx-1 text-blue-600 dark:text-blue-500 cursor-pointer" onClick={() => handleDelete(item.id)}>delete</a>
+                    <a className="font-medium text-blue-600 dark:text-blue-500 cursor-pointer">view</a>
+                </td>
+      </tr>
     </tbody>
-  </table>
+  
   )
 }
 
